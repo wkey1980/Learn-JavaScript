8e743b5e808c782214b30066464255f1288289a5
wkey1980 <28814921+wkey1980@users.noreply.github.com>
2020-11-03 16:15:33 +0000

    commit

--- script.js
+++ script.js
@@ -33,16 +33,73 @@
 // }
 
 
-
-
-
-
-
-
 // let submitBtn = document.getElementById('submit');
 // submitBtn.style = "background-color: green";
 
+// const place = "World";
+// const greeting ="Hello";
+
+// console.log('%s, %s', greeting, place);
+
+
+// console.log(`${greeting}, ${place}`);
+
+// ! NOT Working
+
+// function reverseString(value) {
+//     let reversedValue = "";
+
+//     value.split("").foreach((char) => {
+//         reversedValue = char + reversedValue;
+//     });
+
+//     return reversdeValue;
+// }
+
+// console.log(reverseString("Reverse Me"));
+
+
+// let myFavourateNumber = 7;
+// 10 + myFavourateNumber;
+// let myName = 'Will'
+
+
+// console.log('hello my name is ' + myName + '.';); 
+
+
+/*
+let firstName = 'will';
+let lastName = 'smith';
+
+console.log(firstName + localStorage);
+*/
+
+/*
+function greet() {
+    alert('Hell, my name is will');
+}
+
+greet();
+*/
+/*
+function greet(name, age) {
+    alert("Hello , my name is " + name + "and my age is " + age + ".");
+};
+
+greet('sam', '28');
+*/
+
+/*
+function tripleMe(x) {
+    return 3 * x;
+}
+
+let myFavourateNumber = tripleMe(12);
+
+alert(myFavourateNumber);
+
+*/
 
 
 
-
+// tripleMe();
